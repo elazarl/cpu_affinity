b008d1f6e76ccbad4b22d19e4a50e73b854acb3e
Elazar Leibovich <elazarl@gmail.com>
2014-11-23 15:48:17 -0500

    update script

--- test_linux.sh
+++ test_linux.sh
@@ -7,9 +7,11 @@ killall test_affinity
 PID=$!
 sleep 1
 sudo bash -c "echo $PID > /sys/kernel/debug/tracing/set_ftrace_pid"
+sudo bash -c "> /sys/kernel/debug/tracing/trace"
 sudo bash -c 'echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable'
 sudo bash -c 'cat /sys/kernel/debug/tracing/trace_pipe' | head -n 1000 > test/linux/trace_affine.txt
 sudo bash -c 'echo 0 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable'
+sudo bash -c "> /sys/kernel/debug/tracing/trace"
 ./test/linux/plot_ftrace_sched_switch.py test_affinity <./test/linux/trace_affine.txt >./test/linux/trace_affine.json
 trace2html ./test/linux/trace_affine.json
 
@@ -18,8 +20,10 @@ killall test_affinity
 PID=$!
 sleep 1
 sudo bash -c "echo $PID > /sys/kernel/debug/tracing/set_ftrace_pid"
+sudo bash -c "> /sys/kernel/debug/tracing/trace"
 sudo bash -c 'echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable'
 sudo bash -c 'cat /sys/kernel/debug/tracing/trace_pipe' | head -n 1000 > test/linux/trace_antiaffine.txt
 sudo bash -c 'echo 0 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable'
+sudo bash -c "> /sys/kernel/debug/tracing/trace"
 ./test/linux/plot_ftrace_sched_switch.py test_affinity <./test/linux/trace_antiaffine.txt >./test/linux/trace_antiaffine.json
 trace2html ./test/linux/trace_antiaffine.json
