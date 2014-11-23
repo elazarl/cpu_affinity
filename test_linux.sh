3fb547aa292587b2de81959b5531626c5d3b14e7
Elazar Leibovich <elazarl@gmail.com>
2014-11-23 22:33:01 +0200

    test linux script, added file

---
+++ test_linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./make.sh
+
+killall test_affinity
+./test_affinity affine&
+PID=$!
+sleep 1
+sudo bash -c "echo $PID > /sys/kernel/debug/tracing/set_ftrace_pid"
+sudo bash -c 'echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/active'
+sudo bash -c 'cat /sys/kernel/debug/tracing/trace_pipe' | head -n 1000 > test/linux/trace_affine.txt
+sudo bash -c 'echo 0 > /sys/kernel/debug/tracing/events/sched/sched_switch/active'
+./test/linux/plot_ftrace_sched_switch.py <./test/linux/trace_affine.txt >./test/linux/trace_affine.json
+trace2html ./test/linux/trace_affine.json
+
+killall test_affinity
+./test_affinity antiaffine&
+PID=$!
+sleep 1
+sudo bash -c "echo $PID > /sys/kernel/debug/tracing/set_ftrace_pid"
+sudo bash -c 'echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/active'
+sudo bash -c 'cat /sys/kernel/debug/tracing/trace_pipe' | head -n 1000 > test/linux/trace_antiaffine.txt
+sudo bash -c 'echo 0 > /sys/kernel/debug/tracing/events/sched/sched_switch/active'
+./test/linux/plot_ftrace_sched_switch.py <./test/linux/trace_antiaffine.txt >./test/linux/trace_antiaffine.json
+trace2html ./test/linux/trace_antiaffine.json
